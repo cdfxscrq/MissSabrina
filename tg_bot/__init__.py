d9d9c97ce8fd8455e1723b227dfc7efd415b55b7
AK25 <60230389+cdfxscrq@users.noreply.github.com>
2025-06-05 16:35:59 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -4,91 +4,85 @@ import sys
 
 import telegram.ext as tg
 
-# enable logging
+# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO)
-
+    level=logging.INFO,
+)
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
-    quit(1)
+# Check Python version
+if sys.version_info < (3, 6):
+    LOGGER.error(
+        "You MUST have a Python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    sys.exit(1)
 
-ENV = bool(os.environ.get('ENV', False))
+
+def parse_user_list(user_list):
+    """Parse a space-separated string of IDs into a set of ints, ignoring empty strings."""
+    if not user_list:
+        return set()
+    try:
+        return set(int(x) for x in user_list.split() if x)
+    except ValueError:
+        raise ValueError("User list contains invalid integers.")
+
+
+ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    TOKEN = os.environ.get('TOKEN', None)
+    TOKEN = os.environ.get("TOKEN")
+    if not TOKEN:
+        raise Exception("TOKEN environment variable not set.")
+
     try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', None))
-    except ValueError:
+        OWNER_ID = int(os.environ.get("OWNER_ID"))
+    except (TypeError, ValueError):
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+    MESSAGE_DUMP = os.environ.get("MESSAGE_DUMP")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
 
-    try:
-        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
+    SUDO_USERS = parse_user_list(os.environ.get("SUDO_USERS", ""))
+    SUPPORT_USERS = parse_user_list(os.environ.get("SUPPORT_USERS", ""))
+    WHITELIST_USERS = parse_user_list(os.environ.get("WHITELIST_USERS", ""))
 
-    try:
-        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-
-    try:
-        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    URL = os.environ.get('URL', "")  # Does not contain token
-    PORT = int(os.environ.get('PORT', 5000))
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+    URL = os.environ.get("URL", "")
+    PORT = int(os.environ.get("PORT", 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
 
-    DB_URI = os.environ.get('DATABASE_URL')
-    DONATION_LINK = os.environ.get('DONATION_LINK')
+    DB_URI = os.environ.get("DATABASE_URL")
+    DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
-    DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
-    WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
-    KICK_STICKER = os.environ.get('KICK_STICKER', False)
-    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    API_WEATHER =os.environ.get('API_OPENWEATHER',False) 
-    DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN', "")
-    TEMPORARY_DATA = os.environ.get('TEMPORARY_DATA', None)
-    escape_markdown = os.environ.get('escape_markdown',None)
-    
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    WORKERS = int(os.environ.get("WORKERS", 8))
+    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+    KICK_STICKER = os.environ.get("KICK_STICKER", False)
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    API_OPENWEATHER = os.environ.get("API_OPENWEATHER", False)
+    DEEPFRY_TOKEN = os.environ.get("DEEPFRY_TOKEN", "")
+    TEMPORARY_DATA = os.environ.get("TEMPORARY_DATA")
+    escape_markdown = os.environ.get("escape_markdown")
+
 else:
     from tg_bot.config import Development as Config
+
     TOKEN = Config.API_KEY
     try:
         OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
+    except (TypeError, ValueError):
         raise Exception("Your OWNER_ID variable is not a valid integer.")
 
     MESSAGE_DUMP = Config.MESSAGE_DUMP
     OWNER_USERNAME = Config.OWNER_USERNAME
 
-    try:
-        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
-
-    try:
-        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+    SUDO_USERS = parse_user_list(" ".join(map(str, Config.SUDO_USERS or [])))
+    SUPPORT_USERS = parse_user_list(" ".join(map(str, Config.SUPPORT_USERS or [])))
+    WHITELIST_USERS = parse_user_list(" ".join(map(str, Config.WHITELIST_USERS or [])))
 
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
@@ -107,32 +101,25 @@ else:
     ALLOW_EXCL = Config.ALLOW_EXCL
     API_OPENWEATHER = Config.API_OPENWEATHER
     TEMPORARY_DATA = Config.TEMPORARY_DATA
-    escape_markdown = config.escape_markdown
+    escape_markdown = Config.escape_markdown
+
+# Ensure OWNER_ID and a hardcoded user are in SUDO_USERS
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(594813047)
-    
+SUDO_USERS.add(594813047)  # hardcoded additional sudo user
 
-
-
-
-
-
+# Initialize updater and dispatcher
 updater = tg.Updater(TOKEN, workers=WORKERS)
-
 dispatcher = updater.dispatcher
 
+# Convert sets to lists if needed later (e.g. for handlers)
 SUDO_USERS = list(SUDO_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 
-
-# Load at end to ensure all prev variables have been set
+# Import custom handlers after variables are set
 from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
 
-# make sure the regex handler can take extra kwargs
+# Override telegram.ext handlers if needed
 tg.RegexHandler = CustomRegexHandler
-
 if ALLOW_EXCL:
     tg.CommandHandler = CustomCommandHandler
-
-    
