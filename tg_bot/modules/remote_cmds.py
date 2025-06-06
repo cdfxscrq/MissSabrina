a64ec40a9b335828d00a614810ff9ba0a8875e38
AK25 <60230389+cdfxscrq@users.noreply.github.com>
2025-06-06 13:31:01 +0530

    Update remote_cmds.py

--- tg_bot/modules/remote_cmds.py
+++ tg_bot/modules/remote_cmds.py
@@ -1,17 +1,22 @@
 import html
+import logging
 from typing import Optional, List
 
 from telegram import Message, Chat, Update, Bot, User
 from telegram.error import BadRequest
-from telegram.ext import run_async, CommandHandler, Filters
+from telegram.ext import CommandHandler, Filters
 from telegram.utils.helpers import mention_html
 
 from tg_bot import dispatcher
-from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_ban_protected, can_restrict, \
+from tg_bot.modules.helper_funcs.chat_status import (
+    bot_admin, user_admin, is_user_ban_protected, can_restrict,
     is_user_admin, is_user_in_chat, is_bot_admin
+)
 from tg_bot.modules.helper_funcs.extraction import extract_user_and_text
 from tg_bot.modules.helper_funcs.string_handling import extract_time
 from tg_bot.modules.helper_funcs.filters import CustomFilters
+
+LOGGER = logging.getLogger(__name__)
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",
@@ -27,61 +32,14 @@ RBAN_ERRORS = {
     "Not in the chat"
 }
 
-RUNBAN_ERRORS = {
-    "User is an administrator of the chat",
-    "Chat not found",
-    "Not enough rights to restrict/unrestrict chat member",
-    "User_not_participant",
-    "Peer_id_invalid",
-    "Group chat was deactivated",
-    "Need to be inviter of a user to kick it from a basic group",
-    "Chat_admin_required",
-    "Only the creator of a basic group can kick group administrators",
-    "Channel_private",
-    "Not in the chat"
-}
+RUNBAN_ERRORS = RBAN_ERRORS
+RKICK_ERRORS = RBAN_ERRORS
+RMUTE_ERRORS = RBAN_ERRORS
+RUNMUTE_ERRORS = RBAN_ERRORS
 
-RKICK_ERRORS = {
-    "User is an administrator of the chat",
-    "Chat not found",
-    "Not enough rights to restrict/unrestrict chat member",
-    "User_not_participant",
-    "Peer_id_invalid",
-    "Group chat was deactivated",
-    "Need to be inviter of a user to kick it from a basic group",
-    "Chat_admin_required",
-    "Only the creator of a basic group can kick group administrators",
-    "Channel_private",
-    "Not in the chat"
-}
-
-RMUTE_ERRORS = {
-    "User is an administrator of the chat",
-    "Chat not found",
-    "Not enough rights to restrict/unrestrict chat member",
-    "User_not_participant",
-    "Peer_id_invalid",
-    "Group chat was deactivated",
-    "Need to be inviter of a user to kick it from a basic group",
-    "Chat_admin_required",
-    "Only the creator of a basic group can kick group administrators",
-    "Channel_private",
-    "Not in the chat"
-}
-
-RUNMUTE_ERRORS = {
-    "User is an administrator of the chat",
-    "Chat not found",
-    "Not enough rights to restrict/unrestrict chat member",
-    "User_not_participant",
-    "Peer_id_invalid",
-    "Group chat was deactivated",
-    "Need to be inviter of a user to kick it from a basic group",
-    "Chat_admin_required",
-    "Only the creator of a basic group can kick group administrators",
-    "Channel_private",
-    "Not in the chat"
-}
+def run_async(func):
+    # Dummy decorator for compatibility if using sync handlers
+    return func
 
 @run_async
 @bot_admin
@@ -140,14 +98,12 @@ def rban(bot: Bot, update: Update, args: List[str]):
         message.reply_text("Banned from chat!")
     except BadRequest as excp:
         if excp.message == "Reply message not found":
-            # Do not reply
             message.reply_text('Banned!', quote=False)
         elif excp.message in RBAN_ERRORS:
             message.reply_text(excp.message)
         else:
             LOGGER.warning(update)
-            LOGGER.exception("ERROR banning user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
-                             excp.message)
+            LOGGER.exception("ERROR banning user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id, excp.message)
             message.reply_text("Well damn, I can't ban that user.")
 
 @run_async
@@ -193,7 +149,7 @@ def runban(bot: Bot, update: Update, args: List[str]):
             return
         else:
             raise
-            
+
     if is_user_in_chat(chat, user_id):
         message.reply_text("Why are you trying to remotely unban someone that's already in that chat?")
         return
@@ -207,14 +163,12 @@ def runban(bot: Bot, update: Update, args: List[str]):
         message.reply_text("Yep, this user can join that chat!")
     except BadRequest as excp:
         if excp.message == "Reply message not found":
-            # Do not reply
             message.reply_text('Unbanned!', quote=False)
         elif excp.message in RUNBAN_ERRORS:
             message.reply_text(excp.message)
         else:
             LOGGER.warning(update)
-            LOGGER.exception("ERROR unbanning user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
-                             excp.message)
+            LOGGER.exception("ERROR unbanning user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id, excp.message)
             message.reply_text("Well damn, I can't unban that user.")
 
 @run_async
@@ -274,14 +228,12 @@ def rkick(bot: Bot, update: Update, args: List[str]):
         message.reply_text("Kicked from chat!")
     except BadRequest as excp:
         if excp.message == "Reply message not found":
-            # Do not reply
             message.reply_text('Kicked!', quote=False)
         elif excp.message in RKICK_ERRORS:
             message.reply_text(excp.message)
         else:
             LOGGER.warning(update)
-            LOGGER.exception("ERROR kicking user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
-                             excp.message)
+            LOGGER.exception("ERROR kicking user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id, excp.message)
             message.reply_text("Well damn, I can't kick that user.")
 
 @run_async
@@ -341,14 +293,12 @@ def rmute(bot: Bot, update: Update, args: List[str]):
         message.reply_text("Muted from the chat!")
     except BadRequest as excp:
         if excp.message == "Reply message not found":
-            # Do not reply
             message.reply_text('Muted!', quote=False)
         elif excp.message in RMUTE_ERRORS:
             message.reply_text(excp.message)
         else:
             LOGGER.warning(update)
-            LOGGER.exception("ERROR mute user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
-                             excp.message)
+            LOGGER.exception("ERROR muting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id, excp.message)
             message.reply_text("Well damn, I can't mute that user.")
 
 @run_async
@@ -394,34 +344,37 @@ def runmute(bot: Bot, update: Update, args: List[str]):
             return
         else:
             raise
-            
+
     if is_user_in_chat(chat, user_id):
-       if member.can_send_messages and member.can_send_media_messages \
-          and member.can_send_other_messages and member.can_add_web_page_previews:
-        message.reply_text("This user already has the right to speak in that chat.")
-        return
+        if getattr(member, "can_send_messages", True) and \
+           getattr(member, "can_send_media_messages", True) and \
+           getattr(member, "can_send_other_messages", True) and \
+           getattr(member, "can_add_web_page_previews", True):
+            message.reply_text("This user already has the right to speak in that chat.")
+            return
 
     if user_id == bot.id:
         message.reply_text("I'm not gonna UNMUTE myself, I'm an admin there!")
         return
 
     try:
-        bot.restrict_chat_member(chat.id, int(user_id),
-                                     can_send_messages=True,
-                                     can_send_media_messages=True,
-                                     can_send_other_messages=True,
-                                     can_add_web_page_previews=True)
+        bot.restrict_chat_member(
+            chat.id,
+            int(user_id),
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True,
+        )
         message.reply_text("Yep, this user can talk in that chat!")
     except BadRequest as excp:
         if excp.message == "Reply message not found":
-            # Do not reply
             message.reply_text('Unmuted!', quote=False)
         elif excp.message in RUNMUTE_ERRORS:
             message.reply_text(excp.message)
         else:
             LOGGER.warning(update)
-            LOGGER.exception("ERROR unmnuting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
-                             excp.message)
+            LOGGER.exception("ERROR unmuting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id, excp.message)
             message.reply_text("Well damn, I can't unmute that user.")
 
 __help__ = ""
